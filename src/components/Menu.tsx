2d1f7e6de90acc47609ac3a92dc158d159287b82
thereddas <thereddas@gmail.com>
2024-10-13 00:22:37 +0700

    adding layout and menu

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,3 +1,6 @@
+import Image from "next/image";
+import Link from "next/link";
+
 const menuItems = [
   {
     title: "MENU",
@@ -112,3 +115,29 @@ const menuItems = [
     ],
   },
 ];
+
+const Menu = () => {
+  return (
+    <div className="mt-4 text-sm">
+      {menuItems.map((i) => (
+        <div key={i.title} className="flex flex-col gap-2">
+          <span className="hidden lg:block text-gray-400 font-light my-4">
+            {i.title}
+          </span>
+          {i.items.map((item) => (
+            <Link
+              key={item.label}
+              href={item.href}
+              className="flex items-center justify-center lg:justify-start gap-4 py-2 text-gray-500"
+            >
+              <Image src={item.icon} alt={item.label} width={24} height={24} />
+              <span className="hidden lg:block">{item.label}</span>
+            </Link>
+          ))}
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Menu;
